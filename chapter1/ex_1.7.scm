725ac45f13e087afc1bba5b1edeeda14a3abe4c4
Kartovcev <kartovcev01@mail.ru>
2020-12-01 19:29:45 +0300

    Исправления в решении задания 1.7

--- chapter1/ex_1.7.scm
+++ chapter1/ex_1.7.scm
@@ -1,7 +1,7 @@
 #lang scheme
 
 (define (sqrt x)
-  (sqrt-iter 1.0 1.0 x))
+  (sqrt-iter 0.0 1.0 x))
 (define (sqrt-iter guess-prev guess x)
   (if (good-enough? guess-prev guess)
       guess
@@ -9,4 +9,8 @@
                  (improve guess x)
                  x)))
 (define (good-enough? guess-prev guess)
-  (< (abs (- guess guess-prev)) 0.0001))
+  (< (abs (- guess guess-prev)) 0.00001))
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (average x y)
+  (/ (+ x y) 2))
